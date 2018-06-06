faaf9c68976a25e3a791ea7b78755174f929112d
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-06-06 20:02:45 +0530

    created Recie component and recipesList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
