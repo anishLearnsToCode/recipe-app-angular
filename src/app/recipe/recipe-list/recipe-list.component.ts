96fd120c63fcb6ef33ea07765292811ccc602d46
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-06-08 14:58:22 +0530

    first update

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -10,7 +10,10 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor() {
+    this.recipes.push(new Recipe('Rajma Chawal', 'Tasty Dish',
+      'https://cdn.pixabay.com/photo/2016/08/31/21/47/burger-1634705_960_720.jpg'));
+  }
 
   ngOnInit() {
   }
